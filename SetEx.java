9c68a15bb57399fdcc75f5d5ebd18948a68d8dff
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2022-12-02 22:59:26 +0530

    02-12-2022 Comparator case study

--- SetEx.java
+++ SetEx.java
@@ -1,9 +1,10 @@
 package collections;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-//collections can store both homogeneous/haterogeneous data	
+//collections can store both homogeneous/heterogeneous data	
 //collections are not fixed in size /growable in nature
 //lots of  utility methods	
 
@@ -12,9 +13,9 @@ import java.util.TreeSet;
 //java.util
 public class SetEx {
 	public static void main(String[] args) {
-		TreeSet<String> names = new TreeSet<String>();// 16
+		TreeSet<String> names = new TreeSet<String>(new MyOrder());// 16//NSO java.lang.Comparable-->FI-->compareTo()
 		names.add("sandeep");
-		names.add("nikhila");
+		names.add("nikhila");							//CSO	java.util.Comparator-->FI-->compare
 		names.add("afreen");
 		names.add("rajesh");
 		names.add("laxmi");
@@ -25,6 +26,19 @@ public class SetEx {
 		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-
+		// --return -ve if obj1 has to come before obj2
+		// --return +ve if obj1 has to come after obj2
+		// -- return 0 if obj1 & obj2 are equal.
 	}
+}
+class MyOrder implements Comparator<String>
+{
+
+	@Override
+	public int compare(String o1, String o2) {
+		return o2.compareTo(o1);
+		//return -o1.compareTo(o2);
+	}
+	
+
 }
